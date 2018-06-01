e64294c360c93c6e47ef55ab8b7e6f3714e3f146
roaraurora <dc83080779@gmail.com>
2018-06-01 23:17:16 +0800

    transform to datadrive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../link';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  public links_1: Link[];
+  public links_2: Link[];
   constructor() { }
 
   ngOnInit() {
+    this.links_1 = [
+      { text: '把百度设为首页', url: '' },
+      { text: '关于百度', url: '' },
+      { text: 'About Baidu', url: '' },
+      { text: '百度推广', url: '' },
+    ];
+    this.links_2 = [
+      { text: '©2018 Baidu ', url: '' },
+      { text: '使用百度前必读 ', url: '' },
+      { text: '意见反馈 ', url: '' },
+      { text: '京ICP证030173号', url: '' },
+      { text: '京公网安备1100000020000001号', url: '' },
+    ];
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../link';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public topLinks: Link[];
   constructor() { }
 
   ngOnInit() {
+    this.topLinks = [
+      { text: '新闻', url: '' },
+      { text: 'hao123', url: '' },
+      { text: '地图', url: '' },
+      { text: '视频', url: '' },
+      { text: '贴吧', url: '' },
+      { text: '学术', url: '' },
+      { text: '设置', url: '' },
+    ];
   }
-
 }
